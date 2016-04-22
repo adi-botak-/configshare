9ac96dd2bf4d3adf897a15a423b958b5e40d461b
adi-botak- <adityautamawijaya@gmail.com>
2016-04-22 21:05:20 +0800

    Added tests for configuration creation

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -56,18 +56,44 @@ describe 'Project resource calls' do
 	end
 end
 
-=begin
 describe 'Test creating configuration resources' do 
 	before do
 		Project.dataset.delete
 		Configuration.dataset.delete
+
+		req_header = { 'CONTENT_TYPE' => 'application/json' }
+		req_body = { name: 'Demo Project' }.to_json
+		post '/api/v1/projects/', req_body, req_header
+		@existing_project = Project.first
 	end
 
-	it 'should catch duplicate config files within a project' do
-		#p = Project.create(name: 'class_demo')
-		#p.add_configuration(filename: 'filename.rb' base64_document: 'LS0tCnN1cnZlcl9uYW1lOiBhcGkuZXhhbXBsZS5vcmc=')
-		#duplicate_call = -> { p.add_configuration(filename: 'filename.rb') }
-		#_(duplicate_call).must_raise Sequel::UniqueConstraintViolation 
+	describe 'Adding new configurations to projects' do
+		it 'HAPPY: should add a new configuration for an existing project' do
+			req_header = { 'CONTENT_TYPE' => 'application/json' }
+			req_body = { filename: 'Demo Configuration' }.to_json
+			post "/api/v1/projects/#{@existing_project.id}/configurations", req_body, req_header
+			_(last_response.status).must_equal 201
+			_(last_response.location).must_match(%r{http://})
+		end
+
+		it 'SAD: should not add a configuration for non-existant project' do
+			invalid_id = invalid_id(Project)
+
+			req_header = { 'CONTENT_TYPE' => 'application/json' }
+			req_body = { filename: 'Demo Configuration' }.to_json
+			post "/api/v1/projects/#{invalid_id}/configurations", req_body, req_header
+			_(last_response.status).must_equal 400
+			_(last_response.location).must_be_nil
+		end
+
+		it 'SAD: should catch duplicate config files within a project' do
+			req_header = { 'CONTENT_TYPE' => 'application/json' }
+			req_body = { filename: 'Demo Configuration' }.to_json
+			url = "/api/v1/projects/#{@existing_project.id}/configurations"
+			post url, req_body, req_header
+			post url, req_body, req_header
+			_(last_response.status).must_equal 400
+		end
 	end
 end
 
@@ -76,4 +102,3 @@ describe 'Test idempotent GET routes' do
 		
 	end
 end
-=end
