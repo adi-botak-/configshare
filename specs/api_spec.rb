68d60ae45902f0ff0d711225bd39d528f00deee3
adi-botak- <adityautamawijaya@gmail.com>
2016-04-22 19:41:07 +0800

    .gitignore: omit only files in db/ but not db/migrations/

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -1,6 +1,14 @@
 require_relative './spec_helper'
 require './models/init'
 
+describe 'Test root route' do
+	it 'should find the root route' do
+		get '/'
+		last_response.body.must_include 'ConfigShare'
+		last_response.status.must_equal 200
+	end
+end
+
 describe 'Test creating configuration resources' do 
 	before do
 		Project.dataset.delete
@@ -9,8 +17,14 @@ describe 'Test creating configuration resources' do
 
 	it 'should catch duplicate config files within a project' do
 		p = Project.create(name: 'class_demo')
-		p.add_configuration(filename: 'filename.rb')
+		p.add_configuration(filename: 'filename.rb' base64_document: 'LS0tCnN1cnZlcl9uYW1lOiBhcGkuZXhhbXBsZS5vcmc=')
 		duplicate_call = -> { p.add_configuration(filename: 'filename.rb') }
 		_(duplicate_call).must_raise Sequel::UniqueConstraintViolation 
+	end
+end
+
+describe 'Test idempotent GET routes' do
+	before do
+		
 	end
 end
