a7576182d058ee5b8d8c46ad80e5e09af1ba5a44
adi-botak- <adityautamawijaya@gmail.com>
2016-04-23 08:12:23 +0800

    Used EncryptableModel module for models that need crypto

--- models/configuration.rb
+++ models/configuration.rb
@@ -1,35 +1,20 @@
 require 'json'
 require 'base64'
 require 'sequel'
-require 'rbnacl/libsodium'
 
 # Holds a full configuration file's information
 class Configuration < Sequel::Model
+	include EncryptableModel
+
 	many_to_one :projects
 	set_allowed_columns :filename, :relative_path, :description
 
-	def key
-		@key ||= Base64.strict_decode64(ENV['DB_KEY'])
-	end
-
-	def document=(plaintext)
-		if plaintext
-			secret_box = RbNaCl::SecretBox.new(key)
-			new_nonce = RbNaCl::Random.random_bytes(secret_box.nonce_bytes)
-			ciphertext = secret_box.encrypt(new_nonce, plaintext)
-			self.nonce = Base64.strict_encode64(new_nonce)
-			self.document_encrypted = Base64.strict_encode64(ciphertext)
-		end
+	def document=(document_plaintext)
+		self.document_encrypted = encrypt(document_plaintext)
 	end
 
 	def document
-		@document ||=
-			if document_encrypted
-				secret_box = RbNaCl::SecretBox.new(key)
-				old_nonce = Base64.strict_decode64(nonce)
-				ciphertext = Base64.strict_decode64(document_encrypted)
-				secret_box.decrypt(old_nonce, ciphertext)
-			end
+		@document ||= decrypt(document_encrypted)
 	end
 
 	def to_json(options = {})
