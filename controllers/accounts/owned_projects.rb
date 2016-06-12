897e8f6de8ebf30ae126ba26da37b76b0aa9f806
adi-botak- <adityautamawijaya@gmail.com>
2016-06-12 22:04:45 +0800

    Allow clients to add new projects for owners

--- controllers/accounts/owned_projects.rb
+++ controllers/accounts/owned_projects.rb
@@ -1,31 +1,35 @@
-# Sinatra Application Controllers
+# Owned Projects Controllers
 class ShareConfigurationsAPI < Sinatra::Base
-	post '/api/v1/accounts/:id/owned_projects/?' do 
-		begin
-			new_project_data = JSON.parse(request.body.read)
-			saved_project = CreateProjectForOwner.call(
-				owner_id: params[:id],
-				name: new_project_data['name'],
-				repo_url: new_project_data['repo_url'])
-			new_location = URI.join(@request_url.to_s + '/', saved_project.id.to_s).to_s
-		rescue => e 
-			logger.info "FAILED to create new project: #{e.inspect}"
-			halt 400
-		end
+	post '/api/v1/accounts/:owner_id/projects/?' do 
+	  content_type 'application/json'
 
-		status 201
-		headers('Location' => new_location)
+	  begin
+	    halt 401 unless authorized_account?(env, params[:owner_id])
+
+	    new_project_data = JSON.parse(request.body.read)
+	    saved_project = CreateProjectForOwner.call(
+	      owner_id: params[:owner_id],
+	      name: new_project_data['name'],
+	      repo_url: new_project_data['repo_url'])
+	    new_location = URI.join(@request_url.to_s + '/', saved_project.id.to_s).to_s
+	  rescue => e 
+	    logger.info "FAILED to create new project: #{e.inspect}"
+	    halt 400
+	  end
+
+	  status 201
+	  { type: 'project', link: new_location, data: saved_project }.to_json
 	end
 
-	get '/api/v1/accounts/:owner_id/owned_projects/?' do
-		content_type 'application/json'
+	# get '/api/v1/accounts/:owner_id/owned_projects/?' do
+	# 	content_type 'application/json'
 
-		begin
-			owner = Account[params[:owner_id]]
-			JSON.pretty_generate(data: owner.owned_projects)
-		rescue => e
-			logger.info "FAILED to findprojects for user #{params[:owner_id]}: #{e}"
-			halt 404
-		end
-	end
+	# 	begin
+	# 		owner = Account[params[:owner_id]]
+	# 		JSON.pretty_generate(data: owner.owned_projects)
+	# 	rescue => e
+	# 		logger.info "FAILED to findprojects for user #{params[:owner_id]}: #{e}"
+	# 		halt 404
+	# 	end
+	# end
 end
--- specs/projects_spec.rb
+++ specs/projects_spec.rb
@@ -37,7 +37,7 @@ describe 'Project resource calls' do
 	    	username: 'adi-botak-',
 	    	password: 'adipassword')
 
-	    result = get "/api/v1/accounts/#{@my_account.id}/projects", nil, { "HTTP_AUTHORIZATION" => "Bearer #{auth_token}" }
+	    result = get "/api/v1/accounts/#{@my_account.id}/projects", nil, 'HTTP_AUTHORIZATION' => "Bearer #{auth_token}"
 	    _(result.status).must_equal 200
 	    projs = JSON.parse(result.body)
 
@@ -84,7 +84,7 @@ describe 'Project resource calls' do
 			_, auth_token = AuthenticateAccount.call(
 				username: 'adi-botak-',
 				password: 'adipassword')
-			get "api/v1/projects/#{new_project.id}", nil, { "HTTP_AUTHORIZATION" => "Bearer #{auth_token}" }
+			get "api/v1/projects/#{new_project.id}", nil, 'HTTP_AUTHORIZATION' => "Bearer #{auth_token}"
 			_(last_response.status).must_equal 200
 
 			results = JSON.parse(last_response.body)
@@ -133,24 +133,43 @@ describe 'Project resource calls' do
 				username: 'adi-botak-',
 				email: 'adityautamawijaya@gmail.com',
 				password: 'mypassword')
+
+			_, @auth_token = AuthenticateAccount.call(
+				username: 'adityautamawijaya',
+				password: 'adipassword')
 		end
 
 		it 'HAPPY: should create a new owned project for account' do
-			req_header = { 'CONTENT_TYPE' => 'application/json' }
+			req_header = {
+				'CONTENT_TYPE' => 'application/json',
+				'HTTP_AUTHORIZATION' => "Bearer #{@auth_token}"
+			}
 			req_body = { name: 'Demo Project' }.to_json
 			post "/api/v1/accounts/#{@account.id}/owned_projects/", req_body, req_header
 			_(last_response.status).must_equal 201
-			_(last_response.location).must_match(%r{http://})
+			_(last_response.body).wont_be_empty
 		end
 
 		it 'SAD: should not create projects with duplicate names' do
-			req_header = { 'CONTENT_TYPE' => 'application/json' }
+			req_header = {
+				'CONTENT_TYPE' => 'application/json',
+				'HTTP_AUTHORIZATION' => "Bearer #{@auth_token}"
+			}
 			req_body = { name: 'Demo Project' }.to_json
 			2.times do
 				post "/api/v1/accounts/#{@account.id}/owned_projects/", req_body, req_header
 			end
 			_(last_response.status).must_equal 400
-			_(last_response.location).must_be_nil
+			_(last_response.body).must_be_empty
+		end
+
+		it 'BAD: should not create projects without authorization' do
+		  req_header = { 'CONTENT_TYPE' => 'application.json' }
+		  req_body = { name: 'Demo Project' }.to_json
+		  post "/api/v1/accounts/#{@account.id}/owned_projects/", req_body, req_header
+
+		  _(last_response.status).must_equal 401
+		  _(last_response.location).must_be_nil
 		end
 
 		it 'HAPPY: should encrypt relevant data' do
